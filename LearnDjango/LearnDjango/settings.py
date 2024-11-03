c4aa78f38c23407d9f7b7f0190f6fb658441813d
Kibooto <oleksijc1@gmail.com>
2024-11-03 22:47:43 +0200

    Django-3

--- LearnDjango/LearnDjango/settings.py
+++ LearnDjango/LearnDjango/settings.py
@@ -117,10 +117,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'static')
+]
+
+
 
 
 # Default primary key field type
